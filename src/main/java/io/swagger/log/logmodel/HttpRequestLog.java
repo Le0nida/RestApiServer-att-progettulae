b49691adc26d5fd4785aa86e4ea85a9536ce71ef
leonardo ruberto <leonardorubo@hotmail.com>
2024-05-25 10:22:19 +0200

    Update HttpRequestLog.java

--- src/main/java/io/swagger/log/logmodel/HttpRequestLog.java
+++ src/main/java/io/swagger/log/logmodel/HttpRequestLog.java
@@ -3,6 +3,11 @@ package io.swagger.log.logmodel;
 import javax.persistence.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 
 @Entity
@@ -148,8 +153,32 @@ public class HttpRequestLog {
     }
 
     private String getRequestBody(HttpServletRequest request) {
-        return request.toString();
-        // TODO to implement
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        return stringBuilder.toString();
     }
 
 
