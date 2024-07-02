51564e405d4753f8dd1d02afa6034de469799791
Leonardo Ruberto <leonardo.ruberto@studio.unibo.it>
2024-07-02 12:36:31 +0200

    FIX grafiche Vulnerable controller pages

--- src/main/java/io/swagger/api/VulnerableController.java
+++ src/main/java/io/swagger/api/VulnerableController.java
@@ -4,14 +4,13 @@ import io.swagger.model.User;
 import io.swagger.model.VulnerableUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.ByteArrayInputStream;
-import java.io.ObjectInputStream;
 import java.sql.*;
 import java.util.*;
 
